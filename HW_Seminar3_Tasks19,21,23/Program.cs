a8a620ab16cf5e9dd0e6fb4cf291b6fcc6de5a71
Malvina <malvink@bk.ru>
2022-12-08 23:18:37 +0300

    add task 019

--- HW_Seminar3_Tasks19,21,23/Program.cs
+++ HW_Seminar3_Tasks19,21,23/Program.cs
@@ -37,14 +37,37 @@
 // 5 -> 1, 8, 27, 64, 125
 
 
-Console.WriteLine("Enter N: ");
-int N = int.Parse(Console.ReadLine());
-int i = 1;
-double cube = 1;
+//Console.WriteLine("Enter N: ");
+//int N = int.Parse(Console.ReadLine());
+//int i = 1;
+//double cube = 1;
 
-while (N >= i)
+//while (N >= i)
+//{
+  //  cube = Math.Pow(i,3);
+    //Console.Write($"{cube}, ");
+   // i++;
+//}
+
+// Задача 19.Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите пятизначное число: ");
+string number = Console.ReadLine();
+
+void NumberChoice(string number)
 {
-    cube = Math.Pow(i,3);
-    Console.Write($"{cube}, ");
-    i++;
+  if (number[0]==number[4] || number[1]==number[3])
+  {
+    Console.WriteLine($"Число {number} является палиндромом ");
+  }
+  else Console.WriteLine($"Число {number} число не является палиндромом ");
 }
+if (number!.Length == 5)
+{
+ NumberChoice(number);
+}
+else Console.WriteLine($"Введите правильное число");
+
